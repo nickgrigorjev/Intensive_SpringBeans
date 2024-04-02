eb1e98964ba72729d916adebdb80f384d49c72b9
nickgrigojev <[ns.grigorjev@gmail.com])>
2024-04-02 13:43:05 +0400

    Initial commit

---
+++ src/main/java/app/model/Timer.java
@@ -0,0 +1,17 @@
+package app.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Timer {
+
+    private Long nanoTime = System.nanoTime();
+
+    public Timer() {
+    }
+
+    public Long getTime() {
+        return nanoTime;
+    }
+}
